70a51c122c9ae2df32b64aed2cbb539c82f04d47
David <david.ramon.valle@fau.de>
2024-10-11 19:43:22 +0200

    Node Class creation

---
+++ Node/Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by David Valle on 11-Oct-24.
+//
+
+#include "Node.hpp"
---
+++ Node/Node.hpp
@@ -0,0 +1,14 @@
+//
+// Created by David Valle on 11-Oct-24.
+//
+
+#ifndef UNTITLED3_NODE_HPP
+#define UNTITLED3_NODE_HPP
+
+
+class Node {
+
+};
+
+
+#endif //UNTITLED3_NODE_HPP
